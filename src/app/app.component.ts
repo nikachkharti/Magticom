231c79651d7f0ae93661c67931ecec533103f442
nikachkharti <nika.chkhartishvili7@gmail.com>
2021-07-15 19:36:46 +0400

    navbar search created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Magti-app';
+  hidden: boolean = true;
+
+  showInput()
+  {
+    this.hidden = !this.hidden;
+  }
+
 }
